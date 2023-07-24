869f9b55b65964c80b6a70c884f617ac0c8477c4
Una <hihi555000@gmail.com>
2023-07-24 18:01:14 +0800

    Initial Commit

---
+++ player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  player
+//
+//  Created by 徐于茹 on 2023/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
