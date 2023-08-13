cba45985051af7607d47c3bc695b3039a6175d3f
Una <hihi555000@gmail.com>
2023-08-13 10:56:43 +0800

    feat: 新增music list
    (cherry picked from commit bbcf7fd7882f0e83e50e2d26b670fb247ebbad28)

---
+++ player/Views/SongTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  SongTableViewCell.swift
+//  player
+//
+//  Created by 徐于茹 on 2023/8/9.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var sortLabel: UILabel!
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var songNameLabel: UILabel!
+    
+    @IBOutlet weak var listeningAcountLabel: UILabel!
+    @IBOutlet weak var epNameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ player/models/SongData.swift
@@ -0,0 +1,16 @@
+//
+//  SongData.swift
+//  player
+//
+//  Created by 徐于茹 on 2023/8/9.
+//
+
+import Foundation
+
+struct Song {
+    let name: String
+    let albumPic: String
+    let songFile: String
+    let singer: String
+    let albumName: String
+}
